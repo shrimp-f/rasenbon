58d5920b0dc71af9d91f84d73dc6435f00ce6f22
shrimp-f <fujitadynamics@gmail.com>
2020-05-27 16:55:28 +0900

    2.5

---
+++ 2.5.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+
+int main(void)
+{
+    int R[200000], n;
+    std::cin >> n;
+
+    for (int i=0; i<n; i++) std::cin >> R[i];
+
+    int maxp = -2000000000;
+    int min_R = R[0];
+
+    for (int i=1; i<n; i++){
+        maxp = std::max(maxp, R[i] - min_R);
+        min_R = std::min(min_R, R[i]);
+    std::cout << maxp << std::endl;
+    }
+
+    std::cout << maxp << std::endl;
+
+    return 0;
+}
