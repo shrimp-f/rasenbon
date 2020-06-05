b9248311424fd0ff4e1051ebc19f4daeb6a5536e
shrimp-f <fujitadynamics@gmail.com>
2020-06-05 23:53:41 +0900

    途中

---
+++ 4.3.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+
+#define LEN 100005
+
+typedef struct pp{
+    char name[100];
+    int t;
+}P;
+
+
+int main(){
+    
+}
