f39aa0ebed8364a631009e46fa97738d68d1393d
shrimp-f <fujitadynamics@gmail.com>
2020-06-08 23:40:05 +0900

    4.4

--- 4.4.cpp
+++ 4.4.cpp
@@ -17,7 +17,7 @@ Node *nil;
 
 Node* listSearch(int key){
     Node * cur = nil->next;
-    while(cur != nl && cur->key != key){
+    while(cur != nil && cur->key != key){
         cur = cur->next;
     }
     return cur;
@@ -39,4 +39,70 @@ void printList(){
         cur = cur->next;
     }
     printf("\n");
+}
+
+
+void deleteNode(Node *t){
+    if(t==nil) return;
+    t->prev->next = t->next;
+    t->next->prev = t->prev;
+    free(t);
+}
+
+
+void deleteFirst(){
+    deleteNode(nil->next);
+}
+
+
+void deleteLast(){
+    deleteNode(nil->prev);
+}
+
+
+void deleteKey(int key){
+    deleteNode(listSearch(key));
+}
+
+
+void insert(int key){
+    Node *x = (Node *)malloc(sizeof(Node));
+    x->key = key;
+    x->next = nil->next;
+    nil->next->prev = x;
+    nil->next = x;
+    x->prev = nil;
+}
+
+
+int main(){
+    int key, n, i;
+    int size = 0;
+    char com[20];
+    int np = 0;
+    int nd = 0;
+    
+    scanf("%d", &n);
+    init();
+    for(i=0; i<n; i++){
+        scanf("%s%d", com, &key);
+        if(com[0]=='i'){
+            insert(key);
+            np++;
+            size++;
+        }else if(com[0]=='d'){
+            if(strlen(com)>6){
+                if(com[6]=='F') deleteFirst();
+                else if(com[6]=='L') deleteLast();
+            }else{
+                deleteKey(key);
+                nd++;
+            }
+            size--;
+        }
+    }
+
+    printList();
+
+    return 0;
 }
