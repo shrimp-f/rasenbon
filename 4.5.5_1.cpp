e6dce4c6144e93c9859dbab4172b6db30e6e23da
shrimp-f <fujitadynamics@gmail.com>
2020-06-12 23:48:21 +0900

    4.5.5_1 list

---
+++ 4.5.5_1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int main(){
+    list<char> L;
+
+    L.push_front('b');
+    L.push_back('c');
+    L.push_front('a');
+
+    cout << L.front();
+    cout << L.back();
+
+    L.pop_front();
+    L.push_back('d');
+
+    cout << L.front();
+    cout << L.back() << endl;
+
+    return 0;
+}
