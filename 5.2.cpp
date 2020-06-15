e2bcf337c026486906d43771c8bdba5e1889135c
shrimp-f <fujitadynamics@gmail.com>
2020-06-15 22:49:01 +0900

    5.2 linear search

---
+++ 5.2.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+
+int search(int A[], int n, int key){
+    int i=0;
+    // 配列の末尾に自身と同じ値(番兵)を置くことで目的の値が見つからなかった場合もエラーにならない
+    A[n] = key;
+    while(A[i] != key) i++;
+    return i != n;
+}
+
+
+int main(){
+    int i, n, A[10000+1], q, key, sum=0;
+
+    scanf("%d", &n);
+    for(i=0; i<n; i++) scanf("%d", &A[i]);
+
+    scanf("%d", &q);
+    for(i=0; i<q; i++){
+        scanf("%d", &key);
+        if(search(A, n, key)) sum++;
+    }
+    printf("%d\n", sum);
+
+    return 0;
+}
